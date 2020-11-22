7985c75940cdc2018dcfef188c1969ac873b14a9
J3ykob <jaqbmaliszewski@gmail.com>
2020-11-22 10:54:39 +0100

    done

--- src/views/shopPage.js
+++ src/views/shopPage.js
@@ -7,12 +7,31 @@ import { SearchResult, Navbar, Searchbar } from '../components'
 import '../assets/css/ShopPage.css'
 
 export default function ShopPage() {
-  const [results, setResults] = useState([])
+  const [products, setResults] = useState([])
+  const [shopInfo, setShopInfo] = useState({
+    company_name: 'name',
+    0: 'desc',
+  })
+
   const { id } = useParams()
-  console.log(id)
   useEffect(() => {
     axios.get(`https://stargard.festiwalczacki.pl/backend/get_shop.php?id=${id}`).then(res => {
-      setResults(res.data)
+      let shopData = JSON.parse(res.data.split('[')[1].split(']')[0])
+      let products = []
+      res.data
+        .split('[')[2]
+        .split(']')[0]
+        .split('},{')
+        .forEach((e, i) => {
+          let g = '{' + e.replace('{', '').replace('}', '') + '}'
+          products.push(JSON.parse(g))
+        })
+      console.log(shopData, products)
+
+      console.log(shopInfo)
+
+      setResults(products)
+      setShopInfo(shopData)
     })
   }, [id])
   return (
@@ -20,17 +39,17 @@ export default function ShopPage() {
       <Navbar />
       <img></img>
       <div className='shopDesc'>
-        <h2>NAZWA SKLEPU</h2>
-        <p>OPIS SKLEPU OPIS SKLEPU OPIS SKLEPU OPIS SKLEPU OPIS SKLEPU </p>
+        <h2>{shopInfo.company_name}</h2>
+        <p>{shopInfo[0]}</p>
       </div>
       <div className='tileWrapper'>
-        {results.map(res => (
+        {products.map(res => (
           <SearchResult
             productId={res.id}
             image={res.image}
             name={res.product_name}
-            price={res.price}
-            vendor={res.vendor}
+            price={res.product_price}
+            vendor={shopInfo.company_name}
             desc='morem lipsum'
           />
         ))}
